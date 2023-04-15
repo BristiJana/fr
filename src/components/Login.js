0b80601998551d229822c63390f9824b2be72b03
Bristi Jana <bristi0654@gmail.com>
2023-04-15 20:51:56 +0530

    2nd commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,48 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+import { Card, Image } from "react-bootstrap";
 
 function Login() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState("");
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+
   return (
-    <div>
-      <Link to="/next">
-        <button>FaceBook</button>
-      </Link>
+    <div class="container">
+      <Card style={{ width: "600px" }}>
+        <Card.Header>
+          {!login && (
+            <FacebookLogin
+              appId="111618935219639"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook"
+            />
+          )}
+          {login && <Image src={picture} roundedCircle />}
+        </Card.Header>
+        {login && (
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>{data.email}</Card.Text>
+          </Card.Body>
+        )}
+      </Card>
     </div>
   );
 }
+
 export default Login;
